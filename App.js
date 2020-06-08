e90271c0b86e7bad1375aa5ea9c7e65b9c0f2de1
mouad.zizi <mouadzizi@users.noreply.github.com>
2020-06-08 04:37:54 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import GlobalStyle from './style/GlobalStyle'
+export default function App() {
+  return (
+    <View style={GlobalStyle.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
