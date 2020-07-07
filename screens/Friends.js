139f03d9040bc882b4087ab7fa49b5a8a0ca199f
moussa23 <mosawi.man@live.fr>
2020-07-07 20:02:14 +0100

    friends list is shown

--- screens/Friends.js
+++ screens/Friends.js
@@ -1,14 +1,35 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React,{useState,useEffect} from 'react';
+import {View, Text,FlatList} from 'react-native';
 import {GlobalStyle} from '../style/GlobalStyle';
 import { FAB } from 'react-native-paper';
+import {auth,db} from '../data/firebaseConfig'
 
 
 export default function Friends({navigation}){
+    const uid=auth.currentUser.uid;
+    const[friends,setFriends]=useState([])
+    useEffect(()=>{
+        console.log("friends pages" );
+        
+        db.ref('user/'+uid+'/friends').on('value',(snapShot)=>{
+            let fr=[];
+            snapShot.forEach((child)=>{
+                fr.push({
+                    key:child.key,
+                    name:child.val().username,
+                    email:child.val().email
+                });
+            })
+            setFriends(fr);
+        })
+        return () => db.ref('value').off();
+    },[]);
     return(
     <View style={GlobalStyle.container}>
-        <Text>This is your friends page</Text>
-
+        <FlatList 
+           data={friends}
+           renderItem={({item})=><Text> {item.name} </Text>}
+        />
         <FAB
         onPress={()=>navigation.push('search')}
         style={GlobalStyle.fab}
--- screens/search.js
+++ screens/search.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useState} from 'react';
 import {View,ActivityIndicator,FlatList, Alert } from 'react-native';
-import { Searchbar, List, Divider} from 'react-native-paper';
+import { Searchbar, List, Divider,Avatar } from 'react-native-paper';
 
 import {db,auth} from '../data/firebaseConfig';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -12,15 +12,14 @@ export default function search() {
     const[allUsers,setAllUsers]=useState([]);
     const currentUser=auth.currentUser;
     const[loading,setLoading]=useState(false);
-
+    const[friends,setFriends] = useState([])
 
     useEffect(()=>{
         //start loading
         setLoading(true);
         //fetch data from database
-        
         db.ref('user')
-        .on('value',(dataSnapShot)=>{
+        .once('value',(dataSnapShot)=>{
             let users=[];
             dataSnapShot.forEach((child)=>{
                     if(currentUser.displayName!=child.val().info.username){
@@ -32,11 +31,35 @@ export default function search() {
                 }
             })
             setAllUsers(users);
-            setLoading(false);
+            setLoading(false); 
         })
-       
+        db.ref('user/'+currentUser.uid+'/friends').on('value',(snapShot)=>{
+            let friends=[];
+            snapShot.forEach((child)=>{
+                friends.push(child.val().username) 
+            })
+            setFriends(friends) 
+        })
+     //return () => db.ref('value').off();
     },[])
 
+    function push(fr){
+        db.ref('user/'+currentUser.uid+'/friends').push(fr)
+        .catch(err=>Alert.alert(err))
+    }
+
+    function addFriend(key){
+        console.log(friends+" / "+friends.length);
+        db.ref('user/'+key).once("value",(snapShot)=>{
+            if(friends.includes(snapShot.val().info.username)) {
+                Alert.alert('Error','You cant add this user twice, are you gay?');
+                return;
+            }
+            else{
+              push(snapShot.val().info);
+            }   
+        })
+    }
 
     return (
         <View>
@@ -52,11 +75,14 @@ export default function search() {
                 data={allUsers}
                 renderItem={({item})=>  
                 <TouchableOpacity
-                onPress={console.log("you touched something! touch your dick instead u pervert !!!")}>
+                onPress={()=>addFriend(item.key)}>
                 <List.Item
+                left={props=><Avatar.Text {...props} color={'white'} size={50} 
+                label={item.name.charAt(0)}/>}
                 title= {item.name}         
-                description="your.email.com"
-                right={props => <List.Icon {...props} icon="plus" />} />
+                description={item.email}
+                right={props => <List.Icon {...props}  icon="plus" />} />
+                
                 <Divider />
                 </TouchableOpacity>}/>
         </View>
